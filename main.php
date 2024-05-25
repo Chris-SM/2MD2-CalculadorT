36248fce2da712101e746e9cec19325ce6e0884a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-24 21:58:12 -0300

    feito

---
+++ main.php
@@ -0,0 +1,21 @@
+<?php
+$Numer = $_GET["Num"];
+$Quant = $_GET["Vezes"];
+echo "<center>Tabuada do ",$Numer," até ",$Quant;
+echo "<style> table, th, td { border: 1px solid black;}</style>
+    <table><tr><td style='font-size: large;color: darkblue;background-color: lightgray;'>Calculo</td>
+    <td style='font-size: large;color: darkblue;background-color: lightgray;'>Resultado</td></tr>";
+if($Quant>0){
+    for($A = 0;$A<=$Quant;$A++)
+{
+    echo "<tr><td>",$Numer,"X",$A,"</td><td>",($Numer*$A),"</td></tr>";
+}}
+else
+{
+    for($A = 0;$A>=$Quant;$A--)
+{
+    echo "<tr><td>",$Numer,"X",$A,"</td><td>",($Numer*$A),"</td></tr>";
+}}
+echo "</table><br>";
+echo "<form action='index.html'><input type='submit' value='Voltar'></form></center>"
+?>
